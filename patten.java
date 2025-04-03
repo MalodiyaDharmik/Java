188e733f15331518cbfa5e8df140babffccc0e0d
MalodiyaDharmik <153921437+MalodiyaDharmik@users.noreply.github.com>
2025-04-04 00:02:00 +0530

    Add files via upload

---
+++ patten.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class patten {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       
+       
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int a=1;a<=i;a++){
+                System.out.print("$");
+               
+            }
+            for(int b=2;b<=i;b++){
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+         
+    }
+}
