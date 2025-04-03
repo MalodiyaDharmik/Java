5689460e0cf8d7446facaf81e72ecc8aee0573d8
MalodiyaDharmik <153921437+MalodiyaDharmik@users.noreply.github.com>
2025-04-04 00:00:05 +0530

    Add files via upload

---
+++ diamondPatten.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class diamondPatten {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      
+      for(int i=1;i<=n+1;i++){
+
+
+        for(int j=1;j<=n-i+1+1;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+       for(int j=1;j<=2*(n-i+1);j++){
+        System.out.print(" ");
+       }
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+
+        System.out.println(" ");
+      }
+
+
+
+      for(int i=n;i>=1;i--){
+        
+        
+        for(int j=1;j<=n-i+1+1;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+       for(int j=1;j<=2*(n-i+1);j++){
+        System.out.print(" ");
+       }
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+
+        System.out.println(" ");
+      }
+    }
+}
